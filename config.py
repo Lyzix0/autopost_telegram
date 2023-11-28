78623a63177be81d32b7894a24da9873d1318ac4
Maxim Valiev <123410376+Lyzix0@users.noreply.github.com>
2023-11-28 20:15:45 +0300

    config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-openai_api = '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'
-bot_api = "6100231674:AAEKDmcnij7qr4j-rJtgUyoO9WyslFtAo7g"
-google_api = "AIzaSyABPTywy6mb_BvYRF48u2YW8x201-BViE8"
-cx = "c285c5115db6745c4"
+openai_api = ''
+bot_api = ''
+google_api = ''
+cx = ''
